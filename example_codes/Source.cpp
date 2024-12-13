fa8f4c98d067b21649db45502dcf6edfb5bd601b
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-12-13 09:46:03 +0200

    Update Source.cpp

--- example_codes/Source.cpp
+++ example_codes/Source.cpp
@@ -1,10 +1,72 @@
 #include"Header.h"
 
 int main() {
-    
+    size_t  n ;// Dimension of the system
+    std::cin >> n;
+    assert(n > 0);
+    //ofstream output;
+    //output.open("output.txt", ios::out);
+   /* if (!output.is_open())
+    {
+        cout << "Error opening file";
+        exit(-1);
+    }*/
 
-   
-    finite_difference(1.0, 3.0, 0.1);
-    
-    
+    // Build the x,c,b vector
+    std::vector<double> x(n), c(n), b(n);
+
+    for (size_t i = 0; i < n; i++)
+    {
+        x[i] =static_cast<double>( i);
+        b[i] = f(static_cast<double>(i));
+    }
+
+
+    //Build the left side (A)
+    std::vector<std::vector<double>> A(n, std::vector<double>(n, 0));
+    for (size_t i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            A[i][j] = std::pow(x[i], j);
+        }
+    }
+
+    c = gauss_elim(A, b);
+
+    std::cout << "x:";
+    print_Vector(x);
+    std::cout << '\n';
+
+
+    std::cout << "A matrix:" << '\n';
+    print2DVector(A);
+
+
+    std::cout << "c:" << '\n';
+    print_Vector(c);
+    std::cout << '\n';
+
+
+    //Build the interpolation polynomial
+    double sum = 0;
+    std::vector<double> Pn(n);
+    for (size_t i = 0; i < n; i++)
+    {
+        sum = 0;
+        for (int j = 0; j < n; j++)
+        {
+            sum += c[j] * std::pow(x[i], j);
+        }
+        Pn[i] = sum;
+    }
+    std::cout << "x  " << " Pn(x) " << " f(x) " << '\n';
+    for (size_t i = 0; i < n; i++)
+    {
+        std::cout << "x:" << x[i] << " Pn(x):" << Pn[i] << " f(x)" << f(x[i]) << '\n';
+
+    }
+
+
+    return 0;
 }
