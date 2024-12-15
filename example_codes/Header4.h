a231c19b19c10361fda821d4dc9777d7d19dd778
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-12-15 14:02:38 +0200

    Update Header4.h

--- example_codes/Header4.h
+++ example_codes/Header4.h
@@ -1,26 +1,28 @@
 #pragma once
 #include <iostream>
 #include <cmath>
-#include<concepts>
 #include<cassert>
-template<std::floating_point _Floating>
-[[nodiscard]] inline _Floating f(const _Floating& x) noexcept
+#include<type_traits>
+template<typename _Ty>
+requires(std::disjunction_v<std::is_same<_Ty,float>,std::is_same<_Ty,double>,std::is_same<_Ty,long double>>)
+[[nodiscard]] inline _Ty f(const _Ty& x) noexcept
 {
     //{for pi  #include<numbers> => std::numbers::pi}
     //return pow(2,x) + 5 * x +3;
     return exp(2 * x);
     //return sin(2 * M_PI *x);
 }
-template<std::floating_point _Floating>
-[[nodiscard]] inline _Floating trapezoid_integral(const _Floating& a,const  _Floating& b,const  _Floating& dx) {
+template<typename _Ty>
+requires(std::disjunction_v<std::is_same<_Ty,float>,std::is_same<_Ty,double>,std::is_same<_Ty,long double>>)
+[[nodiscard]] inline _Ty trapezoid_integral(const _Ty& a,const  _Ty& b,const  _Ty& dx) {
     assert(b > a);
     assert(dx > 0);
     const size_t &iterations = static_cast<size_t>((b - a) / dx);
 
-    _Floating result = 0;
-    _Floating xi = a;
+    _Ty result = 0;
+    _Ty xi = a;
     for (size_t i = 0; i < iterations; i++) {
-        result += static_cast<_Floating>((f(xi + dx) + f(xi)) * (dx * 0.5));
+        result += static_cast<_Ty>((f(xi + dx) + f(xi)) * (dx * 0.5));
         std::cout << xi << "-" << xi + dx << " " << result << '\n';
         xi += dx;
     }
