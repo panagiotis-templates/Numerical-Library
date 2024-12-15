53d8eec0ad5aed2d6cddfe28cf3c1c58401c308f
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-12-15 13:58:45 +0200

    Update Header2.h

--- example_codes/Header2.h
+++ example_codes/Header2.h
@@ -1,24 +1,29 @@
 #pragma once
-#include<concepts>
 #include<iostream>
 #include<cmath>
-//#include<concepts>
-template<std::floating_point _Floating>
-inline [[nodiscard]] _Floating  f(const _Floating& x) noexcept
+#include<cassert>
+#include<type_traits>
+template<typename _Ty>
+requires(std::disjunction_v<std::is_same<_Ty,float>,std::is_same<_Ty,double>,std::is_same<_Ty,long double>>)
+[[nodiscard]]  inline _Ty f(const _Ty& x) noexcept
 {
     return std::exp(2 * x);
 }
-template<std::floating_point _Floating>
-inline [[nodiscard]] _Floating ddf(const _Floating& x) noexcept
+template<typename _Ty>
+requires(std::disjunction_v<std::is_same<_Ty,float>,std::is_same<_Ty,double>,std::is_same<_Ty,long double>>)
+[[nodiscard]] inline  _Ty ddf(const _Ty& x) noexcept
 {
     return 4 * std::exp(2 * x);
 }
-template<std::floating_point _Floating>
-inline void finite_difference(const _Floating& a, const  _Floating& b, const  _Floating& h) {
+template<typename _Ty>
+requires(std::disjunction_v<std::is_same<_Ty,float>,std::is_same<_Ty,double>,std::is_same<_Ty,long double>>)
+inline void finite_difference(const _Ty& a, const _Ty& b, const  _Ty& h) {
    
-    _Floating xi{}, res{}, hsq{ std::pow(h, 2) };
+    _Ty xi{}, res{}, hsq{ std::pow(h, 2) };
     const size_t& n{ static_cast<size_t>((b - a) / h + 1) };
     xi = a;
+    assert(b>a);
+    assert(h>0);
     for (size_t i = 0; i < n; i++) {
         res = (f(xi - h) - 2 * f(xi) + f(xi + h)) / hsq;
         std::cout << xi << " " << res << " " << ddf(xi) << " " << std::abs(ddf(xi) - res) << '\n';
