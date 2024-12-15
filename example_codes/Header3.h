2076a0aad21db8e12ee6dda833181549e849b3f2
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-12-15 13:23:41 +0200

    Update Header3.h

--- example_codes/Header3.h
+++ example_codes/Header3.h
@@ -61,28 +61,32 @@ template<std::floating_point _Floating>
 #include<concepts>
 #include<cassert>
 #include<type_traits>
-template<std::floating_point _Floating>
-[[nodiscard]] inline _Floating f(const _Floating& x) noexcept
+template<typename _Ty>
+requires(std::disjunction_v<std::is_same<_Ty,float>,std::is_same<_Ty,double>,std::is_same<_Ty,long double>>)
+[[nodiscard]] inline  _Ty  f(const _Ty& x) noexcept
 {
-    return static_cast<_Floating>(std::pow(x, 3) - 2 * x - 5);
+    return static_cast<_Ty>(std::pow(x, 3) - 2 * x - 5);
     //return x * x + 2 * x;
 }
 
-template<std::floating_point _Floating>
-[[nodiscard]] bool inline isEqual(const _Floating& a, const _Floating& b, const _Floating& epsilon = static_cast<_Floating>(10e-10))noexcept {
+template<typename _Ty>
+requires(std::disjunction_v<std::is_same<_Ty,float>,std::is_same<_Ty,double>,std::is_same<_Ty,long double>>)
+[[nodiscard]] bool inline isEqual(const _Ty& a, const _Ty& b, const _Ty& epsilon = static_cast<_Ty>(10e-10))noexcept {
 
     return std::abs(a - b) < epsilon;
 }
 
-template<std::floating_point _Floating, typename t>
-[[nodiscard]] inline std::optional<_Floating> dicection(_Floating a, _Floating b, t&& f, _Floating e = static_cast<_Floating>(10e-10))
+template<typename _Ty,typename u>
+requires(std::disjunction_v<std::is_same<_Ty,float>,std::is_same<_Ty,double>,std::is_same<_Ty,long double>>)
+[[nodiscard]] inline std::optional<_Ty> dicection(_Ty a, _Ty b, u&& f, _Ty e = static_cast<_Ty>(10e-10))//u is the callable
 {
+
+    static_assert(std::is_same_v<decltype(f(a)), _Ty>, "return type of f must be a floating point type");
+    static_assert(std::is_invocable_r_v<_Ty, u, _Ty>, "3rd argument must be a callable that returns a floating point value and takes only one floating point value");
    
-    static_assert(std::is_same_v<decltype(f(a)), _Floating>, "return type of f must be a floating point type");
-    static_assert(std::is_invocable_r_v<_Floating, t, _Floating>, "3rd argument must be a callable that returns a floating point value and takes only one floating point value");
     assert(b > a);
     assert(e > 0);
-    _Floating d = b - a, c{}; //[a,b] interval ,d interval span ,e precision
+    _Ty d = b - a, c{}; //[a,b] interval ,d interval span ,e precision
     
     while (true)
     {
@@ -92,12 +96,12 @@ template<std::floating_point _Floating, typename t>
             std::cout << "Not found in interval: " << "[" << a << "," << b << "]";
             return std::nullopt;
         }
-        c = static_cast<_Floating>(a + (b - a) * 0.5);
+        c = static_cast<_Ty>(a + (b - a) * 0.5);
         std::cout << a << " " << b << " " << c << " " << d << " " << f(c) << '\n';
         if (std::abs(f(c) - 0) < 10e-6) //Smaller precision for the final result 
         {
 
-             return std::optional{c};
+            return std::optional{c};
         }
         if (std::signbit(f(c)) == std::signbit(f(a))) //if(f(c) * f(a) > 0) is going to produce overflow if f(a) or f(c) is  small number
         {
@@ -112,5 +116,6 @@ template<std::floating_point _Floating, typename t>
         }
     }
 }
+
 
 
