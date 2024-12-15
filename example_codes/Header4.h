1875a2d36724ea31585ed92582314adc1295844e
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-12-15 09:33:20 +0200

    Add files via upload

---
+++ example_codes/Header4.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+#include<concepts>
+#include<cassert>
+template<std::floating_point _Floating>
+[[nodiscard]] inline _Floating f(_Floating x) noexcept
+{
+    //{for pi  #include<numbers> => std::numbers::pi}
+    //return pow(2,x) + 5 * x +3;
+    return exp(2 * x);
+    //return sin(2 * M_PI *x);
+}
+template<std::floating_point _Floating>
+[[nodiscard]] inline _Floating trapezoid_integral(_Floating a, _Floating b, _Floating dx) {
+    assert(b > a);
+    assert(dx > 0);
+    size_t iterations = static_cast<size_t>((b - a) / dx);
+   
+    _Floating result = 0;
+    _Floating xi = a;
+    for (size_t i = 0; i < iterations; i++) {
+        result +=static_cast<_Floating>((f(xi + dx) + f(xi)) * (dx * 0.5));
+        std::cout << xi << "-" << xi + dx << " " << result << '\n';
+        xi += dx;
+    }
+   
+    return result;
+}
+
+
+
+
+
---
+++ example_codes/Source4.cpp
@@ -0,0 +1,10 @@
+#include"Header4.h"
+
+
+int main()
+{
+    float a = 0, b = 1, dx = 0.1f;
+    std::cout<<trapezoid_integral(a, b, dx);
+    
+    return 0;
+}
