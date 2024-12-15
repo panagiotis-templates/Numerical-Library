e2273aa6590f97d9cd3e0acedf57dda1a57087c2
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-12-15 09:35:54 +0200

    Update Header3.h

--- example_codes/Header3.h
+++ example_codes/Header3.h
@@ -1,24 +1,27 @@
+#pragma once
 #include<iostream>
 #include<cmath>
 #include<optional>
 #include<concepts>
-
+#include<cassert>
 template<std::floating_point _Floating>
-[[nodiscard]]inline _Floating f(_Floating x) noexcept
+[[nodiscard]] inline _Floating f(_Floating x) noexcept
 {
     return static_cast<_Floating>(std::pow(x, 3) - 2 * x - 5);
     //return x * x + 2 * x;
 }
 
 template<std::floating_point _Floating>
-[[nodiscard]] bool inline isEqual(const _Floating a, const _Floating b,const _Floating epsilon= static_cast<_Floating>(10e-10))noexcept {
+[[nodiscard]] bool inline isEqual(const _Floating a, const _Floating b, const _Floating epsilon = static_cast<_Floating>(10e-10))noexcept {
 
     return std::abs(a - b) < epsilon;
 }
 
 template<std::floating_point _Floating>
-[[nodiscard]] inline std::optional<_Floating> dicection(_Floating a, _Floating b, _Floating(*f)(_Floating), _Floating e =static_cast<_Floating>( 10e-10))
+[[nodiscard]] inline std::optional<_Floating> dicection(_Floating a, _Floating b, _Floating(*f)(_Floating), _Floating e = static_cast<_Floating>(10e-10))
 {
+    assert(b > a);
+    assert(e > 0);
     _Floating d = b - a, c{}; //[a,b] interval ,d interval span ,e precision
     while (true)
     {
@@ -28,22 +31,22 @@ template<std::floating_point _Floating>
             std::cout << "Not found in interval: " << "[" << a << "," << b << "]";
             return std::nullopt;
         }
-        c =static_cast<_Floating>( a + (b - a) * 0.5);
+        c = static_cast<_Floating>(a + (b - a) * 0.5);
         std::cout << a << " " << b << " " << c << " " << d << " " << f(c) << '\n';
         if (std::abs(f(c) - 0) < 10e-6) //Smaller precision for the final result 
         {
-         
+
             return c;
         }
         if (std::signbit(f(c)) == std::signbit(f(a))) //if(f(c) * f(a) > 0) is going to produce overflow if f(a) or f(c) is  small number
         {
-           
+
             a = c;
-            
+
         }
         else
         {
-           
+
             b = c;
         }
     }
