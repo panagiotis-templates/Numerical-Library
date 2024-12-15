e58c146b5ab6058a561e244e4035d29abd486df4
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-12-15 10:34:12 +0200

    Update Header2.h

--- example_codes/Header2.h
+++ example_codes/Header2.h
@@ -1,28 +1,27 @@
 #pragma once
-
 #include<concepts>
-#include<vector>
 #include<iostream>
-#include<iomanip>
+#include<cmath>
+//#include<concepts>
 template<std::floating_point _Floating>
-inline [[nodiscard]] _Floating  f(_Floating x) noexcept
+inline [[nodiscard]] _Floating  f(const _Floating& x) noexcept
 {
-    return exp(2 * x);
+    return std::exp(2 * x);
 }
 template<std::floating_point _Floating>
-inline [[nodiscard]] _Floating ddf(_Floating x) noexcept
+inline [[nodiscard]] _Floating ddf(const _Floating& x) noexcept
 {
-    return 4 * exp(2 * x);
+    return 4 * std::exp(2 * x);
 }
 template<std::floating_point _Floating>
-inline void finite_difference(_Floating a, _Floating b, _Floating h) {
-    _Floating xi{}, res{}, hsq{};
-    size_t n{ static_cast<size_t>((b - a) / h + 1) };
+inline void finite_difference(const _Floating& a, const  _Floating& b, const  _Floating& h) {
+   
+    _Floating xi{}, res{}, hsq{ std::pow(h, 2) };
+    const size_t& n{ static_cast<size_t>((b - a) / h + 1) };
     xi = a;
-    hsq = std::pow(h, 2);
     for (size_t i = 0; i < n; i++) {
         res = (f(xi - h) - 2 * f(xi) + f(xi + h)) / hsq;
-        std::cout << xi << " " << res << " " << ddf(xi) << " " << fabs(ddf(xi) - res) << '\n';
+        std::cout << xi << " " << res << " " << ddf(xi) << " " << std::abs(ddf(xi) - res) << '\n';
         xi += h;
     }
 
