a8c5f23dffef405fb0d40b37c6f9cfce68a51807
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-12-15 11:04:08 +0200

    Update Header4.h

--- example_codes/Header4.h
+++ example_codes/Header4.h
@@ -4,7 +4,7 @@
 #include<concepts>
 #include<cassert>
 template<std::floating_point _Floating>
-[[nodiscard]] inline _Floating f(_Floating x) noexcept
+[[nodiscard]] inline _Floating f(const _Floating& x) noexcept
 {
     //{for pi  #include<numbers> => std::numbers::pi}
     //return pow(2,x) + 5 * x +3;
@@ -12,21 +12,22 @@ template<std::floating_point _Floating>
     //return sin(2 * M_PI *x);
 }
 template<std::floating_point _Floating>
-[[nodiscard]] inline _Floating trapezoid_integral(_Floating a, _Floating b, _Floating dx) {
+[[nodiscard]] inline _Floating trapezoid_integral(const _Floating& a,const  _Floating& b,const  _Floating& dx) {
     assert(b > a);
     assert(dx > 0);
-    size_t iterations = static_cast<size_t>((b - a) / dx);
-   
+    const size_t &iterations = static_cast<size_t>((b - a) / dx);
+
     _Floating result = 0;
     _Floating xi = a;
     for (size_t i = 0; i < iterations; i++) {
-        result +=static_cast<_Floating>((f(xi + dx) + f(xi)) * (dx * 0.5));
+        result += static_cast<_Floating>((f(xi + dx) + f(xi)) * (dx * 0.5));
         std::cout << xi << "-" << xi + dx << " " << result << '\n';
         xi += dx;
     }
-   
+
     return result;
 }
+
 
 
 
