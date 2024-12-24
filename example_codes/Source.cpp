fec89b6739c5f7740657d0dd3a21b2ff67bdf8b5
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-12-24 19:39:43 +0200

    Update Source.cpp

--- example_codes/Source.cpp
+++ example_codes/Source.cpp
@@ -1,7 +1,7 @@
 #include"Header.h"
 
 int main() {
-    size_t  n ;// Dimension of the system
+    size_t  n;// Dimension of the system
     std::cin >> n;
     assert(n > 0);
     //ofstream output;
@@ -13,11 +13,11 @@ int main() {
     }*/
 
     // Build the x,c,b vector
-    std::vector<double> x(n), c(n), b(n);
+    std::vector<double> x(n), b(n);
 
     for (size_t i = 0; i < n; i++)
     {
-        x[i] =static_cast<double>( i);
+        x[i] = static_cast<double>(i);
         b[i] = f(static_cast<double>(i));
     }
 
@@ -26,13 +26,13 @@ int main() {
     std::vector<std::vector<double>> A(n, std::vector<double>(n, 0));
     for (size_t i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (size_t j = 0; j < n; j++)
         {
             A[i][j] = std::pow(x[i], j);
         }
     }
 
-    c = gauss_elim(A, b);
+    auto c = gauss_elim(A, b);
 
     std::cout << "x:";
     print_Vector(x);
@@ -41,32 +41,34 @@ int main() {
 
     std::cout << "A matrix:" << '\n';
     print2DVector(A);
+    if (c.has_value()) {
+
+        std::cout << "c:" << '\n';
+        print_Vector(c.value());
+        std::cout << '\n';
 
 
-    std::cout << "c:" << '\n';
-    print_Vector(c);
-    std::cout << '\n';
-
-
-    //Build the interpolation polynomial
-    double sum = 0;
-    std::vector<double> Pn(n);
-    for (size_t i = 0; i < n; i++)
-    {
-        sum = 0;
-        for (int j = 0; j < n; j++)
+        //Build the interpolation polynomial
+        double sum = 0;
+        std::vector<double> Pn(n);
+        for (size_t i = 0; i < n; i++)
         {
-            sum += c[j] * std::pow(x[i], j);
+            sum = 0;
+            for (size_t j = 0; j < n; j++)
+            {
+                sum += c->at(j) * std::pow(x[i], j);
+            }
+            Pn[i] = sum;
         }
-        Pn[i] = sum;
-    }
-    std::cout << "x  " << " Pn(x) " << " f(x) " << '\n';
-    for (size_t i = 0; i < n; i++)
-    {
-        std::cout << "x:" << x[i] << " Pn(x):" << Pn[i] << " f(x)" << f(x[i]) << '\n';
+        std::cout << "x  " << " Pn(x) " << " f(x) " << '\n';
+        for (size_t i = 0; i < n; i++)
+        {
+            std::cout << "x:" << x[i] << " Pn(x):" << Pn[i] << " f(x)" << f(x[i]) << '\n';
+
+        }
 
     }
-
-
+    return 0;
+}
     return 0;
 }
