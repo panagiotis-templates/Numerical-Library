5b2e92bf36026fc136fd49a787921591a95ea7de
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-12-26 23:03:35 +0200

    Update Source5.cpp

--- example_codes/Source5.cpp
+++ example_codes/Source5.cpp
@@ -1,8 +1,8 @@
 #include"Header5.h"
 
 int main() {
-   
-    std::cout<<simpson(-1.0,1.0,0.01);
-    
-    
+
+    std::cout << simpson(-1.0, 1.0, 0.01,[](const double &x){  return std::exp(2 * x); });
+
+
 }
