3355fc5f91d563d921c8fb0e2594ba54ed38c33b
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-12-26 23:03:15 +0200

    Update Header5.h

--- example_codes/Header5.h
+++ example_codes/Header5.h
@@ -1,4 +1,36 @@
 #pragma once
+#include <iostream>
+#include <cmath>
+#include<cassert>
+#include<type_traits>
+#include<utility>
+template<typename _Ty>
+struct is_decimal :std::disjunction<std::is_same<_Ty, float>, std::is_same<_Ty, double>, std::is_same<_Ty, long double>> {};
+
+template<typename _Ty>
+inline constexpr bool is_decimal_v = is_decimal <_Ty>::value;
+
+
+
+template<typename _Ty,typename u>
+requires(is_decimal_v<_Ty>)
+[[nodiscard]] _Ty simpson(const _Ty& a,const  _Ty& b,const  _Ty& dx,u&&f) {
+    size_t  iterations = static_cast<size_t>((b - a) / dx);
+    _Ty result = 0;
+    _Ty xi = a;
+
+    for (size_t i = 0; i < iterations; i++)
+    {
+        result += (dx / 6) * (std::invoke(f,xi) + 4 *std::invoke( f, (xi + (xi + dx)) * 0.5) + std::invoke(f,xi+dx));
+        std::cout << xi << "-" << xi + dx << " " << result << '\n';
+        xi += dx;
+    }
+
+    return result;
+}
+
+
+/*#pragma once
 #include<iostream>
 #include<cmath>
 #include<type_traits>
@@ -27,3 +59,4 @@ requires(std::disjunction_v<std::is_same<_Ty, float>, std::is_same<_Ty, double>,
    
     return result;
 }
+*/
