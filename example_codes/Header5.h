ca3041861e2aa08a8d97f75cc8b8cf93ec0d4ba4
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-12-27 21:33:56 +0200

    Update Header5.h

--- example_codes/Header5.h
+++ example_codes/Header5.h
@@ -16,13 +16,20 @@ inline constexpr bool is_decimal_v = is_decimal <_Ty>::value;
 template<typename _Ty,typename u>
 requires(is_decimal_v<_Ty>)
 [[nodiscard]] _Ty simpson(const _Ty& a,const  _Ty& b,const  _Ty& dx,u&&f) {
-    size_t  iterations = static_cast<size_t>((b - a) / dx);
+    static_assert(is_decimal_v<std::invoke_result_t<decltype(f), _Ty>>, "return type of f must be a floating point type");
+    static_assert(std::is_invocable_r_v<_Ty, u, _Ty>, "4rd argument must be a callable that returns a floating point value and takes only one floating point value");
+    if (b <= a || dx <= 0) {
+        std::cerr << "b>a &&dx>0" << '\n';
+        return;
+    }
+   
+    const size_t&  iterations = static_cast<size_t>((b - a) / dx);
     _Ty result = 0;
     _Ty xi = a;
 
     for (size_t i = 0; i < iterations; i++)
     {
-        result += (dx / 6) * (std::invoke(f,xi) + 4 *std::invoke( f, (xi + (xi + dx)) * 0.5) + std::invoke(f,xi+dx));
+        result += (dx / 6) * (std::invoke(f, xi) + 4 * std::invoke(f, (xi + (xi + dx)) * 0.5) + std::invoke(f, xi + dx));
         std::cout << xi << "-" << xi + dx << " " << result << '\n';
         xi += dx;
     }
