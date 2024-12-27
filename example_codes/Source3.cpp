5731506a7330b2a81a7a7367f10a93967519b227
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-12-27 20:37:53 +0200

    Update Source3.cpp

--- example_codes/Source3.cpp
+++ example_codes/Source3.cpp
@@ -3,10 +3,10 @@
 int main()
 {
     double a = 2, b = 3, d = b - a, e = 10e-10; //[a,b] interval ,d interval span ,e precision 
-    if (auto c = dicection(a, b, f<long double>); c.has_value()) {
-        std::cout << *c << '\n';
-    }
-    return 0;
+ if (auto c = dicection(a, b, [](const double& x) {return std::pow(x, 3) - 2 * x - 5; }); c.has_value()) {
+     std::cout << *c << '\n';
+ }
+ return 0;
     
 
 }
