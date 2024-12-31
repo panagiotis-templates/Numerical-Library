7630037093bb2c8495197cb6a6ed662775639dc1
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-12-31 14:54:05 +0200

    Update Header2.h

--- example_codes/Header2.h
+++ example_codes/Header2.h
@@ -6,29 +6,29 @@
 #include<utility>
 #include<functional>
 template<typename _Ty>
-struct is_decimal :std::disjunction<std::is_same<_Ty, float>, std::is_same<_Ty, double>, std::is_same<_Ty, long double>> {};
+
+inline constexpr bool is_decimal_v = std::disjunction_v<std::is_same<_Ty, float>, std::is_same<_Ty, double>, std::is_same<_Ty, long double>>;
 
 template<typename _Ty>
-inline constexpr bool is_decimal_v = is_decimal <_Ty>::value;
+struct is_decimal :std::bool_constant<is_decimal_v<_Ty>>{};//tag dispatching must support it
 
-
-template<typename _Ty,typename u,typename v>
+template<typename _Ty, typename u, typename v>
 requires(is_decimal_v<_Ty>)
-inline void finite_difference(const _Ty& a, const _Ty& b, const  _Ty& h,u&&f,v&&ddf) {
-   static_assert(is_decimal_v<std::invoke_result_t<decltype(f), _Ty>>, "return type of f  must be a floating point type");
-   static_assert(std::is_invocable_r_v<_Ty, u, _Ty>, "4th argument must be a callable that returns a floating point value and takes only one floating point value");
+inline void finite_difference(const _Ty& a, const _Ty& b, const  _Ty& h, u&& f, v&& ddf) {
+    static_assert(is_decimal_v<std::invoke_result_t<decltype(f), _Ty>>, "return type of f  must be a floating point type");
+    static_assert(std::is_invocable_r_v<_Ty, u, _Ty>, "4th argument must be a callable that returns a floating point value and takes only one floating point value");
 
-   static_assert(is_decimal_v<std::invoke_result_t<decltype(ddf), _Ty>>, "return type of ddf must be a floating point type");
-   static_assert(std::is_invocable_r_v<_Ty, v, _Ty>, "5th argument must be a callable that returns a floating point value and takes only one floating point value");
-   _Ty xi{}, res{}, hsq{ static_cast<_Ty>(std::pow(h, 2)) };
+    static_assert(is_decimal_v<std::invoke_result_t<decltype(ddf), _Ty>>, "return type of ddf must be a floating point type");
+    static_assert(std::is_invocable_r_v<_Ty, v, _Ty>, "5th argument must be a callable that returns a floating point value and takes only one floating point value");
+    _Ty xi{}, res{}, hsq{ static_cast<_Ty>(std::pow(h, 2)) };
     if (b <= a || h <= 0) {
         std::cerr << "b>a &&h>0" << '\n';
-    return;
+        return;
     }
-
+    
     const size_t& n{ static_cast<size_t>((b - a) / h + 1) };
     xi = a;
-
+    
     for (size_t i = 0; i < n; i++) {
         res = (std::invoke(f, xi - h) - 2 * std::invoke(f, xi) + std::invoke(f, xi + h)) / hsq;
         std::cout << xi << " " << res << " " << std::invoke(ddf, xi) << " " << std::abs(std::invoke(ddf, xi) - res) << '\n';
