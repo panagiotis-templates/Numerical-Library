f2fdd6e4adf8b4cbb104ece57c51040ff0329324
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-12-31 14:57:21 +0200

    Update Header5.h

--- example_codes/Header5.h
+++ example_codes/Header5.h
@@ -6,24 +6,25 @@
 #include<utility>
 #include<functional>
 template<typename _Ty>
-struct is_decimal :std::disjunction<std::is_same<_Ty, float>, std::is_same<_Ty, double>, std::is_same<_Ty, long double>> {};
+
+inline constexpr bool is_decimal_v = std::disjunction_v<std::is_same<_Ty, float>, std::is_same<_Ty, double>, std::is_same<_Ty, long double>>;
 
 template<typename _Ty>
-inline constexpr bool is_decimal_v = is_decimal <_Ty>::value;
+struct is_decimal :std::bool_constant<is_decimal_v<_Ty>>{};//tag dispatching must support it
 
 
 
-template<typename _Ty,typename u>
-requires(is_decimal_v<_Ty>)
-[[nodiscard]] _Ty simpson(const _Ty& a,const  _Ty& b,const  _Ty& dx,u&&f) {
+template<typename _Ty, typename u>
+    requires(is_decimal_v<_Ty>)
+[[nodiscard]] std::optional<_Ty> simpson(const _Ty& a, const  _Ty& b, const  _Ty& dx, u&& f) {
     static_assert(is_decimal_v<std::invoke_result_t<decltype(f), _Ty>>, "return type of f must be a floating point type");
     static_assert(std::is_invocable_r_v<_Ty, u, _Ty>, "4rd argument must be a callable that returns a floating point value and takes only one floating point value");
     if (b <= a || dx <= 0) {
         std::cerr << "b>a &&dx>0" << '\n';
-        return -23;
+        return std::nullopt;
     }
-   
-    const size_t&  iterations = static_cast<size_t>((b - a) / dx);
+
+    const size_t& iterations = static_cast<size_t>((b - a) / dx);
     _Ty result = 0;
     _Ty xi = a;
 
@@ -34,7 +35,7 @@ requires(is_decimal_v<_Ty>)
         xi += dx;
     }
 
-    return result;
+    return std::optional{ result };
 }
 
 
