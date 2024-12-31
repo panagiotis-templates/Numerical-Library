a24eb94bbdb3aee1ae9ea97859d1a6f7591b5ef2
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-12-31 14:59:29 +0200

    Update Header7.h

--- example_codes/Header7.h
+++ example_codes/Header7.h
@@ -4,20 +4,21 @@
 #include<optional>
 #include<cmath>
 template<typename _Ty>
-struct is_decimal :std::disjunction<std::is_same<_Ty, float>, std::is_same<_Ty, double>, std::is_same<_Ty, long double>> {};
+
+inline constexpr bool is_decimal_v = std::disjunction_v<std::is_same<_Ty, float>, std::is_same<_Ty, double>, std::is_same<_Ty, long double>>;
 
 template<typename _Ty>
-inline constexpr bool is_decimal_v = is_decimal <_Ty>::value;
+struct is_decimal :std::bool_constant<is_decimal_v<_Ty>>{};//tag dispatching must support it
 
 template<typename _Ty>
 requires(is_decimal_v<_Ty>)
-[[nodiscard]]  inline std::optional<_Ty> better_sum(const _Ty& n)noexcept
+[[nodiscard]] inline std::optional<_Ty> better_sum(const _Ty& n)noexcept
 {
     if (n <= 0) {
         std::cerr << "b must be greater than zero\n";
         return std::nullopt;
     }
-    
+
     _Ty sum = 1. / (std::pow(n, 2) + n);
     for (size_t i = 1; i < n; i++)
     {
