26c6efe48642b3ca5c0098bda522249e778e2fef
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-12-31 14:58:35 +0200

    Update Header6.h

--- example_codes/Header6.h
+++ example_codes/Header6.h
@@ -5,10 +5,12 @@
 #include<type_traits>
 #include<cassert>
 template<typename _Ty>
-struct is_decimal :std::disjunction<std::is_same<_Ty, float>, std::is_same<_Ty, double>, std::is_same<_Ty, long double>> {};
+
+inline constexpr bool is_decimal_v = std::disjunction_v<std::is_same<_Ty, float>, std::is_same<_Ty, double>, std::is_same<_Ty, long double>>;
 
 template<typename _Ty>
-inline constexpr bool is_decimal_v = is_decimal <_Ty>::value;
+struct is_decimal :std::bool_constant<is_decimal_v<_Ty>>{};//tag dispatching must support it
+
 template<typename _Ty>
 requires(is_decimal_v<_Ty>)
 [[nodiscard]] inline  bool  isEqual(const _Ty& a, const _Ty& b,
@@ -21,36 +23,35 @@ requires(is_decimal_v<_Ty>)
 
 template<typename _Ty>
 requires(is_decimal_v<_Ty>)
-[[nodiscard]] inline  bool strict_diagonal_dominace(const std::vector<std::vector<_Ty>>& A)noexcept //Pass it as refrence so you dont need copy,also add const if you dont goining to modify
+[[nodiscard]]inline  std::optional<bool> strict_diagonal_dominace(const std::vector<std::vector<_Ty>>& A)noexcept //Pass it as refrence so you dont need copy,also add const if you dont goining to modify
 {
-     _Ty sum = 0;
- size_t n = A.size();
- if (A.size() <= 0) {
-     std::cerr << "vector must be nxn with n>0" << '\n';
-     return 0;
- }
+    _Ty sum = 0;
+    size_t n = A.size();
+    if (A.size() <= 0) {
+        std::cerr << "vector must be nxn with n>0" << '\n';
+        return std::nullopt;
+    }
 
- for (size_t i = 0; i < A.size(); i++)
- {
-     sum = 0;//Reset the sum for new row
-     if (A[i].size() != A.size()) {
-         std::cerr << "vector must be nxn with n>0" << '\n';
-         return 0;
-     }
-    
-     for (size_t j = 0; j < A[i].size(); j++)
-     {
-         if (i != j) { sum += A[i][j]; } //Sum the values except the diagonal
-         else { continue; }
-     }
-     if (std::abs(A[i][i]) < std::abs(sum) || isEqual(std::abs(A[i][i]), std::abs(sum))) //Check if one diagonal element is smaller than the sum
-     {
-         return false;
-     }
- }
- return true;
+    for (size_t i = 0; i < A.size(); i++)
+    {
+        sum = 0;//Reset the sum for new row
+        if (A[i].size() != A.size()) {
+            std::cerr << "vector must be nxn with n>0" << '\n';
+            return std::nullopt;
+        }
+
+        for (size_t j = 0; j < A[i].size(); j++)
+        {
+            if (i != j) { sum += A[i][j]; } //Sum the values except the diagonal
+            else { continue; }
+        }
+        if (std::abs(A[i][i]) < std::abs(sum) || isEqual(std::abs(A[i][i]), std::abs(sum))) //Check if one diagonal element is smaller than the sum
+        {
+            return std::optional{ false };
+        }
+    }
+    return std::optional{ true };
 }
-
 
 
 
