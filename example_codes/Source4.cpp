fde771e4a3e780890cf6b8cc3252b05589c474f4
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2024-12-31 14:45:29 +0200

    Update Source4.cpp

--- example_codes/Source4.cpp
+++ example_codes/Source4.cpp
@@ -4,7 +4,7 @@
 int main()
 {
      double a = 0, b = 1, dx = 0.1;
- std::cout << trapezoid_integral(a, b, dx, [](const double& x) {return std::exp(2 * x); });
-
- return 0;
+     if(auto u=trapezoid_integral(a, b, dx, [](const double& x) {return std::exp(2 * x); });u.has_value()){
+        std::cout<<u.value();
+     }
 }
