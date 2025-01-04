64aa5270a664ae646bb868f4a36e34c40b89be79
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-01-05 01:46:30 +0200

    Update Header10.h

--- example_codes/Header10.h
+++ example_codes/Header10.h
@@ -18,6 +18,32 @@ requires(is_decimal_v<_Ty>)
     return std::abs(a - b) < epsilon;
 }
 
+template <typename _Ty>
+requires(is_decimal_v<_Ty>)
+inline void print2DVector(const std::vector<std::vector<_Ty>>& vec) {
+    const size_t& numRows = vec.size();
+    const size_t& numCols = vec[0].size();
+   
+    if (!(vec.size() > 0 && vec[0].size() > 0)) {
+        std::cerr << "vec.size() > 0 && vec[0].size() > 0" << '\n';
+        return;
+    }
+    // Print column headers
+    std::cout << std::setw(12) << " ";
+    for (size_t col = 0; col < numCols; ++col) {
+        std::cout << std::setw(12) << col;
+    }
+    std::cout << '\n';
+    // Print row headers and vector contents
+    for (size_t row = 0; row < numRows; ++row) {
+        std::cout << std::setw(12) << row;
+        for (size_t col = 0; col < numCols; ++col) {
+            std::cout << std::setw(12) << std::fixed << std::setprecision(6) << vec[row][col] << "("
+                << row << "," << col << ")";
+        }
+        std::cout << '\n';
+    }
+}
 template<typename _Ty>
 requires(is_decimal_v<_Ty>)
 [[nodiscard]] inline  std::optional<std::vector<_Ty>> Cholesky_method(const std::vector<std::vector<_Ty>>&A, const std::vector<_Ty>& b) {
