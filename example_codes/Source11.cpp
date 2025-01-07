6d6be2a795c7a78c932140c83312357e605e9694
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-01-07 13:01:35 +0200

    Update Source11.cpp

--- example_codes/Source11.cpp
+++ example_codes/Source11.cpp
@@ -2,9 +2,11 @@
 
 int main()
 {
-    finite_diff_central(0.0, 1.0, 0.1, 
-        [](double t,double q) {return ( (-0.25 * std::exp(-2 * t)) * (-(std::pow(std::numbers::pi, 2) - 16) * std::sin((std::numbers::pi / 2) * t) - 8 * std::numbers::pi * std::cos((std::numbers::pi / 2) * t)) + q * std::exp(-2 * t) * std::sin((std::numbers::pi / 2) * t)); },
-        [](double t) { return ( std::exp(-2 * t) * std::sin((std::numbers::pi / 2) * t)); });
+     if (auto u = finite_diff_central(0.0, 1.0, 0.1,
+     [](double t, double q) {return ((-0.25 * std::exp(-2 * t)) * (-(std::pow(std::numbers::pi, 2) - 16) * std::sin((std::numbers::pi / 2) * t) - 8 * std::numbers::pi * std::cos((std::numbers::pi / 2) * t)) + q * std::exp(-2 * t) * std::sin((std::numbers::pi / 2) * t)); },
+     [](double t) { return (std::exp(-2 * t) * std::sin((std::numbers::pi / 2) * t)); }); u) {
+     std::cout << u.value() << '\n';
+     }
     
     
     return 0;
