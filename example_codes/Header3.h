a9737b6d6fca2dcc0268b1d9c95d2b552b1403bc
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-01-10 22:21:59 +0200

    Update Header3.h

--- example_codes/Header3.h
+++ example_codes/Header3.h
@@ -21,12 +21,12 @@ requires(is_decimal_v<_Ty>)
 }
 template<typename _Ty, typename u>
 requires(is_decimal_v<_Ty>)
-[[nodiscard]]inline std::optional<_Ty> dicection(_Ty a, _Ty b, u&& f, _Ty e = static_cast<_Ty>(1.0E-10))//u is the callable
+[[nodiscard]] inline std::optional<_Ty> dicection(_Ty a, _Ty b, u&& f, _Ty e = static_cast<_Ty>(1.0E-10))//u is the callable
 {
-    static_assert(std::is_same_v<std::invoke_result_t<decltype(f), _Ty>,_Ty>, "return type of f  must be the same with a,b");
+    static_assert(is_decimal_v<std::invoke_result_t<decltype(f), _Ty>>, "return type of f must be a floating point type");
     static_assert(std::is_invocable_r_v<_Ty, u, _Ty>, "3rd argument must be a callable that returns a floating point value and takes only one floating point value");
 
-    if (b <= a || e <= 0) {
+    if (b < a||isEqual<_Ty>(b,a) || e < 0||isEqual<_Ty>(e,0.0)) {
         return std::nullopt;
     }
     _Ty d = b - a, c{}; //[a,b] interval ,d interval span ,e precision
