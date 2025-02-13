6326fb84fd9062fe7183e931fc5f134bc83a7e95
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-02-13 14:17:07 +0200

    Create Source13.cpp

---
+++ example_codes/Source13.cpp
@@ -0,0 +1,16 @@
+#include"Header13.h"
+int main()
+{
+
+    // Define the knots vector
+    std::vector<float> kns = { 00,0,0,0.25,0.5,0.75,1,1,1 };
+    //std::vector<double> kns = {0.0,  1.0, 2.0, 3.0};
+   
+
+    // Define the order of the B-spline
+    int order = 3;
+    basic_calc_plot(kns, order);
+    
+
+    return 0;
+}
