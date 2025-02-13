da5b62581b589fe78017d9728c6381cb620a7aba
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-02-13 14:30:48 +0200

    Update Source13.cpp

--- example_codes/Source13.cpp
+++ example_codes/Source13.cpp
@@ -3,7 +3,7 @@ int main()
 {
 
     // Define the knots vector
-    std::vector<float> kns = { 00,0,0,0.25,0.5,0.75,1,1,1 };
+    std::vector<double> kns = { 00,0,0,0.25,0.5,0.75,1,1,1 };
     //std::vector<double> kns = {0.0,  1.0, 2.0, 3.0};
    
 
