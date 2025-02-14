3af7183e4d06112f9d933f009a47e9bf2daac25e
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-02-14 12:04:27 +0200

    Update Macros.h

--- example_codes/Macros.h
+++ example_codes/Macros.h
@@ -12,6 +12,13 @@
 #define _PANAGIOTIS panagiotis
 
 #if __cplusplus>201703L
+template<typename _Ty>
+
+inline constexpr bool is_double_or_long_double_v = std::disjunction_v<std::is_same<_Ty, double>, std::is_same<_Ty, long double>>;
+
+template<typename _Ty>
+struct is_double_or_long_double :std::bool_constant<is_double_or_long_double_v<_Ty>> {};//tag dispatching must support it
+
 
 template<typename _Ty>
 
@@ -35,6 +42,9 @@ public:
     }
 };
 
+
+
+#endif
 
 
 #endif
